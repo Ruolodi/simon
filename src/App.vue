b60f62b1c7a6cbed5627cbe75e87fb802ba941e4
Ruolodi <killer777hunter@gmail.com>
2022-02-06 20:35:47 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="card" click=""></div>
+    <div class="card" click=""></div>
+    <div class="card" click=""></div>
+    <div class="card" click=""></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arrComp: [],
+      arrUser: [],
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style></style>
