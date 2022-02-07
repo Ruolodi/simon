aa51a9ec7c7d9633d422659b79a20d118b4bc358
Ruolodi <killer777hunter@gmail.com>
2022-02-07 16:45:09 +0300

    commitLogic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="container">
-    <div class="card" click=""></div>
-    <div class="card" click=""></div>
-    <div class="card" click=""></div>
-    <div class="card" click=""></div>
-  </div>
+ <div class="mainBlock">
+    <div class="container">
+      <div class="card" @click="clickUser(1)"></div>
+      <div class="card1" @click="clickUser(2)"></div>
+      <div class="card2" @click="clickUser(3)"></div>
+      <div class="card3" @click="clickUser(4)"></div>
+    </div>
+     <button class="start" @click="start">
+      Start
+      </button>
+ </div>
+  
+  
+ 
 </template>
 
 <script>
@@ -15,7 +23,16 @@ export default {
       arrUser: [],
     }
   },
-  methods: {},
+  methods: {
+    clickUser(id){
+      this.arrUser.push(id)
+      console.log(this.arrUser)
+    },
+    start(){
+      let num = getRandomInt(4)
+      console.log(num)
+    }
+  },
 }
 </script>
 
